f69070a91e8070e7cc6b175256b227a25e0b4709
Ben <ben@rocketmakers.com>
2020-10-18 20:10:15 +0100

    Add lazy brush package and logic to allow for smooth drawing

--- web/src/views/home/home.tsx
+++ web/src/views/home/home.tsx
@@ -4,66 +4,65 @@ import * as React from "react";
 import * as handpose from "@tensorflow-models/handpose";
 import * as tf from "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs-backend-webgl";
+import { LazyBrush, Point } from "lazy-brush";
 
 /// to get round weird export issue
-var { Webcam } = require("react-webcam");
+var Webcam = require("react-webcam");
 import "./home.scss";
 import { Button } from "@rocketmakers/armstrong";
 import { CanvasHelper } from "../../helpers/canvas";
+import { useCanvas } from "../../hooks/useCanvas";
 
 export const HomeView: React.FC = (props) => {
   const webcamRef = React.useRef(null);
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const modelRef = React.useRef(null);
 
-  const rafID = React.useRef(null);
-
-  const landmarksRealTime = async (video, model, ctx) => {
-    async function frameLandmarks() {
-      CanvasHelper.clearCanvas(
-        ctx,
-        video,
-        canvasRef.current.width,
-        canvasRef.current.height
-      );
-
-      const predictions = await model.estimateHands(video);
-
-      if (predictions.length > 0) {
-        // console.log(predictions);
-        const result = predictions[0].landmarks;
-        CanvasHelper.drawFingerTips(result, ctx, ["pinky"]);
-      }
-      rafID.current = requestAnimationFrame(frameLandmarks);
-    }
-
-    frameLandmarks();
-  };
+  const lazy = React.useRef(
+    new LazyBrush({
+      radius: 10,
+      enabled: true,
+      initialPoint: {
+        x: 0,
+        y: 0,
+      },
+    })
+  );
 
   const detect = async () => {
     const net = await handpose.load();
+    modelRef.current = net;
     console.log("Handpose model loaded.");
 
-    // Get Video Properties
+    // // Get Video Properties
     const video = webcamRef.current.video;
     const videoWidth = webcamRef.current.video.videoWidth;
     const videoHeight = webcamRef.current.video.videoHeight;
 
-    // Set canvas height and width
+    // // Set canvas height and width
     canvasRef.current.width = videoWidth;
     canvasRef.current.height = videoHeight;
-
-    // Draw mesh
-    const ctx = canvasRef.current.getContext("2d");
-    // ctx.beginPath();
-    // ctx.moveTo(0, 0);
-    landmarksRealTime(video, net, ctx);
   };
 
-  React.useEffect(() => {
-    return () => {
-      cancelAnimationFrame(rafID.current);
-    };
-  }, [webcamRef?.current?.video]);
+  const draw = async (ctx: CanvasRenderingContext2D) => {
+    if (!modelRef.current || !webcamRef.current) {
+      return;
+    }
+
+    const video = webcamRef.current.video;
+    // CanvasHelper.clearCanvas(
+    //   ctx,
+    //   video,
+    //   canvasRef.current.width,
+    //   canvasRef.current.height
+    // );
+    const predictions = await modelRef.current.estimateHands(video);
+    if (predictions.length > 0) {
+      const result = predictions[0].landmarks;
+      CanvasHelper.drawFingerTips(result, ctx, ["indexFinger"], lazy.current);
+    }
+  };
+
+  const canvasRef = useCanvas(draw);
 
   const changeColor = (color: React.CSSProperties["color"]) => {
     canvasRef.current.getContext("2d").fillStyle = color;
